42f890f460012e3e8e4746f1c0b5b690487ddacf
Anjana Sharma <anjana.sharma@srnext.in>
2025-02-04 01:24:53 +0530

    add email functionality for users

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,31 @@
 class GroupsController < ApplicationController
-  # before_action :authenticate_user!
-  
+  before_action :authenticate_user!
+  before_action :set_group, only: [:show]
+
   def gift_generator;end
 
   def index
-    @groups = Group.includes(participants: { user: { wishlists: :items } }).all
+    # @groups = Group.includes(participants: { user: { wishlists: :items } }).all
+    # @groups = current_user.groups
+    # render json: @groups
+  end 
+
+  def show
+    binding.pry
+    @group = current_user.groups.find(params[:id])
+    render json: {
+      user_email: current_user.email,
+      group_name: @group.group_name,
+      event_date: @group.event_date,
+      amount: @group.budget
+    }
   end 
 
   def create
     @group = Group.new(group_params)
 
     if @group.save
+      GroupEmailJob.perform_later(@group.id)
       render json: { success: "Group created successfully!"}, status: :created
     else
       render json: { errors: @group.errors.full_messages }, status: :unprocessable_entity
@@ -18,6 +33,10 @@ class GroupsController < ApplicationController
   end
 
   private 
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
 
   def group_params
     params.require(:group).permit(
@@ -28,5 +47,5 @@ class GroupsController < ApplicationController
       user_attributes: [:email, :name], 
       participants_attributes: [user_attributes: [:email]]
     )
-  end
+  end 
 end
